Fixed cepstrum normalization. Added more samples.

tensorFlow.py
@@ -17,7 +17,7 @@ test_set = test[:,1:];
 
 model = tf.keras.Sequential([
 # Adds a densely-connected layer with 64 units to the model:
-layers.Dense(64, activation='sigmoid', input_shape=(train_set.shape[1],)),
+layers.Dense(64, activation='relu', input_shape=(train_set.shape[1],)),
 # Add another:
 layers.Dense(128,activation='relu'),
 # Add a softmax layer with 10 output units:
@@ -38,8 +38,30 @@ pred = model.predict(test_set, batch_size=1)
 
 test_chris = np.genfromtxt('sets/chris_test.csv', delimiter=',').T
 pred_chris = model.predict(test_chris, batch_size=1)
+print('\n\nChris Martin\'s speech:')
+print('Probability of acceptance:\t'+str(np.mean(pred_chris, axis=0)[0]))
+print('Probability of rejection:\t'+str(np.mean(pred_chris, axis=0)[1]))
+
+test_chris2 = np.genfromtxt('sets/chris_test2.csv', delimiter=',').T
+pred_chris = model.predict(test_chris, batch_size=1)
+print('\n\nChris Martin\'s speech 2:')
 print('Probability of acceptance:\t'+str(np.mean(pred_chris, axis=0)[0]))
 print('Probability of rejection:\t'+str(np.mean(pred_chris, axis=0)[1]))
 
 test_cillian = np.genfromtxt('sets/cillian_test.csv', delimiter=',').T
 pred_cillian = model.predict(test_cillian, batch_size=1)
+print('\n\nCillian Murphy\'s speech:')
+print('Probability of acceptance:\t'+str(np.mean(pred_cillian, axis=0)[0]))
+print('Probability of rejection:\t'+str(np.mean(pred_cillian, axis=0)[1]))
+
+test_tilda = np.genfromtxt('sets/tilda_test.csv', delimiter=',').T
+pred_tilda = model.predict(test_tilda, batch_size=1)
+print('\n\nTilda Swinton\'s speech:')
+print('Probability of acceptance:\t'+str(np.mean(pred_tilda, axis=0)[0]))
+print('Probability of rejection:\t'+str(np.mean(pred_tilda, axis=0)[1]))
+
+test_jesse = np.genfromtxt('sets/jesse_test.csv', delimiter=',').T
+pred_jesse= model.predict(test_jesse, batch_size=1)
+print('\n\nJesse Eisemberg\'s speech:')
+print('Probability of acceptance:\t'+str(np.mean(pred_jesse, axis=0)[0]))
+print('Probability of rejection:\t'+str(np.mean(pred_jesse, axis=0)[1]))